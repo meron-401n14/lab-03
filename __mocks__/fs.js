91af95d4f56ae4f64ccb4add15ddbebc0465c3b0
meron <malemu17@yahoo.com>
2019-10-09 12:04:33 -0700

    update

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,11 +1,21 @@
 "use strict";
 
 module.exports = exports = {};
+let fileContents = '';
 
-exports.readFile = (file, cb) => {
+fs.readFile = (file, cb) => {
 	if (file.match(/bad/i)) {
 		cb("Invalid File");
 	} else {
 		cb(undefined, new Buffer("File Contents"));
 	}
 };
+
+exports.writeFile = (file, buffer, cb)=> {
+	if(file.match(/bad/i)){
+		cb('Invalid File');
+	} else {
+		fileContents = buffer;
+		cb(undefind, true);
+	}
+}
---
+++ index.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const fsCallback = require("./lib/file-promise.js");
+const fsPromise = require("./lib/file-promise.js");
+const file = process.argv.slice(2)[0];
+
+
+function onDoneReading(err, data) {
+  if (err){
+    console.log.error(err);
+  }else {
+ data.lastName = "callback";
+
+ writeFile(file, data, onDoneWriting)
+ 
+}
+}
+
+
+  
+function readFile(file){
+  fs.readFile(file, onDoneReading);
+}
+
+function onDoneWriting(err, data){
+  
+   if (err){
+     console.error(err);
+   }else {
+     fsCallback.read(file, (err, afterData)=>{
+       cb(afterData);
+     });
+   }
+}
+
+const useCallbacks = cb => {
+   fsCallback.read(file, onDoneReading)
+  };
+ 
+
+
+// Promise 
+
+const usePromise =() =>{
+  return fsPromise
+    .read(file)
+    .then(data => {
+      data.lastName = "promise";
+      return data;
+
+    })
+    .then (obj => fsPromise.write(file, obj))
+    .then(result => fsPromise.read(file))
+    .catch(err => {
+      throw err;
+    });
+};
+
+
+
+const useAsync = async () => {
+  let before = await fsPromise.read(file);
+  before.lastName = "Async";
+
+  await fsPromise.write(file, before);
+  let after = await fsPromise.read(file);
+
+  return after;
+};
+
+
+usePromise()
+  .then(obj => {
+    console.log("Promise:",  obj);
+    return useAsync();
+  })
+
+  .then(obj => {
+    console.log("Async:", obj);
+    useCallbacks(obj => console.log("CB", obj));
+  })
+    .catch(err => console.error("ERR", err));
+
